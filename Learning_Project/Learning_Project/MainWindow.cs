543dcbbee726373caa62bfb931cc1ba416b14c23
Thomas Claasens <thomas.claasens@outlook.com>
2015-04-07 12:18:25 +0200

    Adding project files

---
+++ Learning_Project/Learning_Project/MainWindow.cs
@@ -0,0 +1,21 @@
+using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{	
+	public MainWindow (): base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+	
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void buttonClick1 (object o, Gtk.ClientEventArgs args)
+	{
+		throw new NotImplementedException ();
+	}
+}
